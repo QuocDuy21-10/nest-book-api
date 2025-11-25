93a6e49be298778fef3646388be367a9f36d284c
QuocDuy-2110 <quocduy37na@gmail.com>
2025-11-25 16:08:39 +0700

    feat(databases): create sample users with role ADMIN and USER, ensure INIT_PASSWORD fallback

--- src/databases/databases.module.ts
+++ src/databases/databases.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DatabasesService } from './databases.service';
 import { DatabasesController } from './databases.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forFeature([
+      
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [DatabasesController],
   providers: [DatabasesService],
 })
